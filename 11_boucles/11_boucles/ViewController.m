86ff29774b43bce39dc669b5b90511564c1a3aef
mupacif <mupacif@gmail.com>
2017-04-14 10:49:17 +0200

    ajout lorempixel@08 methododes + débuts boucles

--- 11_boucles/11_boucles/ViewController.m
+++ 11_boucles/11_boucles/ViewController.m
@@ -16,7 +16,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSArray* voitures = @[@"Aston Martin DB10",
+                          @"Bougatti Veyron",
+                          @"Hennessey Venom"];
+    
+    NSLog(@"Mes voitures de colléctions");
+    for(int i=0; i <voitures.count ; i++)
+    {
+        NSString * voiture = [voitures objectAtIndex:i];
+        NSLog(@"%@", voiture);
+    }
+    NSLog(@"Mes voitures affichées avec un for étendu:");
+    for(NSString*voiture in voitures)
+    {
+        NSLog(@"%@", voiture);
+    }
+    
+    NSLog(@"Mes voitures affichées avec une boucle while");
+    //usage d'un itérateur (interface collection d'objects qui dispose
+    //d'une méthode next
+    //une fois à la fin l'itérateur est épuisée
+    //il faut recréer un nouveau
+    NSEnumerator* enumérateur =  [voitures objectEnumerator];
+    id element;
+    while((element = enumérateur.nextObject) != nil )
+    {
+        NSLog(@"%@", element);
+    }
+    
+    NSLog(@"Mes voitures affichées avec une boucles do-while");
+    enumérateur = [voitures objectEnumerator];
+    element = [enumérateur nextObject];
+    do
+    {
+        NSLog(@"%@",element);
+    }while((element = enumérateur.nextObject) != nil);
+    
 }
 
 
