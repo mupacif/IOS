70b65282529901f22c45b7fc2773cdc18904c47e
mupacif <mupacif@gmail.com>
2017-05-03 14:38:52 +0200

    swift optional

--- 03_Swift_fondamentaux/01_fondamentauxSwift/01_fondamentauxSwift/02_optionals.playground/Contents.swift
+++ 03_Swift_fondamentaux/01_fondamentauxSwift/01_fondamentauxSwift/02_optionals.playground/Contents.swift
@@ -1,1 +1,18 @@
 //optional
+
+//swift ne permet pas l'utilisation d'une variable
+//non initialisée
+//parfois cette initialisation nep eut pas être
+//faire, à un moment donnée
+//Dans ce cas, on utilise un ptional
+
+//un optional est une enveloppe qui peut contenir
+//-une valeur ou référence d'un objet
+//-out rien du tout
+
+//Très souvent , les fonctions/Méthode/ initialsateur
+//retourner un optional
+//Exemple
+
+var nomber = Int("121");
+
